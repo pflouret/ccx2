dd513572969832d6b68d445e0951124574b5687e
pablo flouret <quuxbaz@gmail.com>
2008-12-09 23:19:46 +0100

    never too late to add the signals.py file, double doh

---
+++ src/ccx2/signals.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2008, Pablo Flouret <quuxbaz@gmail.com>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met: Redistributions of
+# source code must retain the above copyright notice, this list of conditions and
+# the following disclaimer. Redistributions in binary form must reproduce the
+# above copyright notice, this list of conditions and the following disclaimer in
+# the documentation and/or other materials provided with the distribution.
+# Neither the name of the software nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# quick and dirty signals
+# FIXME: the quick and dirty part
+
+_signals = {}
+
+def register(name):
+  if name in _signals:
+    raise ValueError("Signal with name %r already registered" % name)
+  _signals[name] = []
+
+def connect(name, callback, user_data=None):
+  try:
+    _signals[name].append((callback, user_data))
+  except KeyError:
+    raise NameError("No signal named %r" % name)
+
+def disconnect(name, callback, user_data=None):
+  try:
+    _signals[name].remove((callback, user_data))
+  except KeyError:
+    pass
+
+def emit(name, *args):
+  try:
+    for callback, user_data in _signals[name]:
+      argsc = [a for a in args]
+      if user_data:
+        argsc.append(user_data)
+      callback(*argsc)
+  except KeyError:
+    raise NameError("No signal named %r" % name)
+
