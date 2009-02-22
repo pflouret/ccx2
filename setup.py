04b4a74d03e8a8009d871a3867c27fb5dda13038
pablo flouret <quuxbaz@gmail.com>
2009-02-22 21:20:09 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from glob import glob
 
 setup(name='ccx2',
       version='0.1',
-      description='ncurses client for xmms2',
+      description='console client for xmms2',
       author='Pablo Flouret',
-      author_email='pflouret@gmail.com',
+      author_email='quuxbaz@gmail.com',
       url='',
       download_url='',
       packages=['ccx2'],
       package_dir={'ccx2': 'src/ccx2'},
       scripts=['scripts/ccx2'],
-      requires=['pyparsing', 'urwid'],
-      data_files=[('share/ccx2', glob('share/**'))]
+      requires=['urwid'],
+      #data_files=[('share/ccx2', glob('share/**'))]
      )
 
