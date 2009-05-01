9dffe71bb3c11ba78ae3c54e82860765bda1cacd
pablo flouret <quuxbaz@gmail.com>
2009-05-01 17:49:51 +0200

    merge upstream urwid changes
    2009-03-19  Ian  <Ian Ward>
    	* urwid/container.py:
    	untab container.py
    	[20e3abbcb228]
    
    2009-03-29  Ian  <Ian Ward>
    	* test_urwid.py, urwid/decoration.py:
    	fix Padding bugs, remove no-longer relavant tests
    	[86375be03a2c]
    
    2009-03-29  Ian  <Ian Ward>
    	* urwid/font.py:
    	add backtick to two fonts
    	[814faf5422d2]
    
    2009-03-29  Ian  <Ian Ward>
    	* urwid/listbox.py:
    	Fix missing calculate_filler in listbox.py
    	[cbfc2ab7e7f4]
    
    2009-03-29  Ian  <Ian Ward>
    	* urwid/raw_display.py:
    	raw_display: Update _next_timout when set_input_timeouts is called,
    	add flush() to modify_terminal_palette
    	[0e97027f6c7e]
    
    2009-04-05  Ian  <Ian Ward>
    	* urwid/main_loop.py:
    	new GenericMainLoop class
    	[ae8323368453]
    
    2009-04-05  Ian  <Ian Ward>
    	* urwid/container.py:
    	new WidgetContainer class
    	[183ade91b2f3]
    
    2009-04-05  Ian  <Ian Ward>
    	* urwid/container.py:
    	fix for Overlay containing fixed widget sending wrong size to pack()
    	[d83f85b1f0b1]
    
    2009-04-06  Ian  <Ian Ward>
    	* urwid/widget.py:
    	add "defines" for filler alignment modes
    	[8bd5fe46a291]
    
    2009-04-06  Ian  <Ian Ward>
    	* urwid/widget.py:
    	leave input text as unicode in Edit wigets
    	[7eeb29bb12f2] [tip]

--- src/ccx2/urwid/widget.py
+++ src/ccx2/urwid/widget.py
@@ -46,6 +46,11 @@ LEFT = 'left'
 RIGHT = 'right'
 CENTER = 'center'
 
+# Filler alignment modes 
+TOP = 'top'
+MIDDLE = 'middle'
+BOTTOM = 'bottom'
+
 # Text wrapping modes
 SPACE = 'space'
 ANY = 'any'
@@ -970,8 +975,6 @@ class Edit(Text):
         p = self.edit_pos
         if self.valid_char(key):
             self._delete_highlighted()
-            if type(key) == type(u""):
-                key = key.encode("utf-8")
             self.insert_text( key )
             
         elif key=="tab" and self.allow_tab:
